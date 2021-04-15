25e53190dfe6e64678e2d17d932b7f72ffad02b2
FabianVegaA <fabianveal@gmail.com>
2021-04-15 11:34:52 -0400

    It was added the logics to mutate the vocals and the some consonants except l and r

--- src/word_mutator.py
+++ src/word_mutator.py
@@ -1,6 +1,11 @@
-from random import choice, randint
+from random import choice, randint, shuffle
 
 from string import ascii_lowercase
+
+from pylabeador import syllabify
+
+
+_VOCALS = {'a', 'e', 'i', 'o', 'u'}
 
 
 class WordMutator():
@@ -28,14 +33,21 @@ class WordMutator():
 
         return word
 
-    # TODO: Need to implement the merge of syllables
     def _mutation_per_syllables(self, word):
         syllables = self._splits_syllables(word)
 
-        mutated_syllables = self._mutator_syllables(
-            choice(syllables, self.mutations))
+        syllables = enumerate(syllables)
+        to_mutate = choice(syllables, self.mutations)
+        unmutated_syllables = list(set(syllables)-set(to_mutate))
+        mutated_syllables = self._mutator_syllables(to_mutate)
 
-        return word
+        syllables = list(map(lambda ss: ss[1],
+                             self._sort_syllables(unmutated_syllables + mutated_syllables)))
+
+        if self.reorganize:
+            syllables = shuffle(syllables)
+
+        return ''.join(syllables)
 
     def _mutation_random(self, word):
 
@@ -47,10 +59,68 @@ class WordMutator():
             word = ''.join(l_word)
         return word+'\n'
 
-    # TODO: Need to implement the logic of syllables separation
-    def _splits_syllables(self, word):
-        pass
+    def _sort_syllables(self, syllables):
+        return sorted(syllables, key=lambda s: s[0])
 
-    # TODO: Need to implement the logic of mutation of syllables
+    def _splits_syllables(self, word):
+        return syllabify(word)
+
     def _mutator_syllables(self, syllables):
-        pass
+
+        if self.mutator_consonants:
+            syllables = [self._mutate_consonants(
+                syllable) for syllable in syllables]
+        elif self.mutator_vocals:
+            syllables = [self._mutate_vocals(syllable)
+                         for syllable in syllables]
+
+        return syllables
+
+    def _mutate_consonants(self, syllable):
+        letters = list(enumerate(list(letters)))
+
+        consonants = list(filter(lambda l: not l[1] in _VOCALS, letters))
+
+        # TODO: This does not take a into account the specials cases
+        def mutate_consonant(consonant):
+            return choice(list(set(ascii_lowercase) - _VOCALS))
+
+        if letters[1][1] in ['r', 'l']:
+            continue
+        else:
+            mutated_consonants = list(
+            map(lambda c: (c[0], mutate_consonant(c[1]))))
+
+        syllable = sorted(list(set(letters) - set(consonants)) +
+                          mutated_consonants, key=lambda s: s[0])
+
+        return ''.join(syllable)
+
+    def _mutate_vocals(self, syllable):
+        letters = list(enumerate(list(letters)))
+
+        vocals = list(filter(lambda l: l[1] in _VOCALS, letters))
+
+        def mutate_vocal(vocal):
+            return choice(_VOCALS)
+
+        if letters[0][1] in ['q', 'g']:
+            if len(syllable) == 3 and syllable[1] == 'q' and vocals[1] == 'u':
+                mutated_vocals = list(map(lambda v: (v[0], v[1] if v[1] == 'u' else choice('e', 'i')
+                                                     ), vocals))
+            elif len(syllable) == 3 and syllable[1] == 'g' and vocals[1] == 'u':
+                mutated_vocals = choice(
+                    # Choice between gue y gui
+                    list(map(lambda v: (v[0], v[1] if v[1] == 'u' else choice(
+                        _VOCALS)), choice('e', 'i'))),
+                    # Choice between ga,ge,gi,go,gu
+                    [(vocals[0][0], choice(_VOCALS))]
+                )
+        else:
+            mutated_vocals = list(
+                map(lambda v: (v[0], mutate_vocal(v[1])), vocals))
+
+        syllable = sorted(list(set(letters) - set(vocals)) +
+                          mutated_vocals, key=lambda s: s[0])
+
+        return ''.join(syllable)
